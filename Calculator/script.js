642cf81c0c0ba8d8c615d8946441e9a9a3809f5e
Rupesh Kumar <23it182@skcet.ac.in>
2025-03-06 17:25:40 +0530

    completed

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,20 +1,20 @@
-// Append values to the display
+
 function appendToDisplay(value) {
     document.getElementById("display").value += value;
 }
 
-// Clear display
+
 function clearDisplay() {
     document.getElementById("display").value = "";
 }
 
-// Delete last entered character
+
 function deleteLast() {
     let current = document.getElementById("display").value;
     document.getElementById("display").value = current.slice(0, -1);
 }
 
-// Evaluate expression
+
 function calculateResult() {
     try {
         document.getElementById("display").value = eval(document.getElementById("display").value);
